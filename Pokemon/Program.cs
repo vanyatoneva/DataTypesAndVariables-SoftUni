126e2a870ee3df9d4168df250785d5ca63de8141
Vanya Vanyova <vanyattoneva@gmail.com>
2022-09-24 23:58:53 +0300

    MoreExercises

---
+++ Pokemon/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Pokemon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            int remaining = n;
+            int targets = 0;
+
+            while(remaining >= m)
+            {
+                targets++;
+                remaining -= m;
+                if(remaining != 0 && n / remaining == 2 && n % remaining == 0 && y != 0)
+                {
+                    remaining /= y;
+                }
+            }
+
+            Console.WriteLine(remaining);
+            Console.WriteLine(targets);
+        }
+    }
+}
---
+++ SpiceMustFlow/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SpiceMustFlow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int yield = int.Parse(Console.ReadLine());
+            int days = 0;
+            ulong extractedSpices = 0;
+
+            while(yield >= 100)
+            {
+                days++;
+                extractedSpices += (ulong)yield - 26;
+                if(extractedSpices < 0)
+                {
+                    extractedSpices = 0;
+                }
+                yield -= 10;
+            }
+
+            if (extractedSpices >= 26)
+            {
+                extractedSpices -= 26; //workers get 26 more when ready
+            }
+            else
+            {
+                extractedSpices = 0;
+            }
+            Console.WriteLine(days);
+            Console.WriteLine(extractedSpices);
+        }
+    }
+}
